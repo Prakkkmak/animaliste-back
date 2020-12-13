e32c5b4d5f258f97b660c8a75f660cf640224c19
Lévy Marques <prakkmak@gmail.com>
2020-12-13 15:27:53 +0100

    Projet généré

---
+++ src/main/kotlin/com/progreizh/animaliste/AnimalisteApplication.kt
@@ -0,0 +1,11 @@
+package com.progreizh.animaliste
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnimalisteApplication
+
+fun main(args: Array<String>) {
+    runApplication<AnimalisteApplication>(*args)
+}
---
+++ src/test/kotlin/com/progreizh/animaliste/AnimalisteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.progreizh.animaliste
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnimalisteApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

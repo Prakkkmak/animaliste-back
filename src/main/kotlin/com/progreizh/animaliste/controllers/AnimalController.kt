c6e362f740258f337b66acb77c5882d885ce8a3c
blamour <b.lamour03@gmail.com>
2020-12-18 17:35:54 +0100

    Ajout des requêtes PUT & DELETE & tests sur les requête POST & DELETE

--- src/main/kotlin/com/progreizh/animaliste/controllers/AnimalController.kt
+++ src/main/kotlin/com/progreizh/animaliste/controllers/AnimalController.kt
@@ -2,7 +2,6 @@ package com.progreizh.animaliste.controllers
 
 import com.progreizh.animaliste.entities.Animal
 import com.progreizh.animaliste.repositories.AnimalRepository
-import org.bson.types.ObjectId
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -24,13 +23,35 @@ class AnimalController(private val repository: AnimalRepository) {
      */
     @GetMapping("/{id}")
     fun getOneAnimals(@PathVariable("id") id: String): ResponseEntity<Animal> {
-        val animal = repository.findOneById(ObjectId(id))
-        return ResponseEntity.ok(animal)
+        val animalOptional = repository.findById(id)
+        return if (animalOptional.isEmpty)
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        else
+            ResponseEntity.ok(animalOptional.get())
     }
 
     @PostMapping
-    fun createAnimal(@RequestBody animal: Animal) : ResponseEntity<Animal> {
-        val animal = repository.save(animal)
+    fun createAnimal(@RequestBody animal: Animal): ResponseEntity<Animal> {
+        val newAnimal = repository.insert(animal)
+        return ResponseEntity(newAnimal, HttpStatus.CREATED)
+    }
+
+    @PutMapping("/{id}")
+    fun putAnimal(@PathVariable("id") id: String, @RequestBody newAnimal: Animal): ResponseEntity<Animal> {
+        val animal = repository.save(newAnimal)
         return ResponseEntity(animal, HttpStatus.CREATED)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteAnimal(@PathVariable("id") id: String): ResponseEntity<Animal> {
+        val animalOptional = repository.findById(id)
+        return if (animalOptional.isEmpty)
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        else {
+            repository.delete(animalOptional.get())
+            ResponseEntity.ok(animalOptional.get())
+        }
+
+
     }
 }
--- src/test/kotlin/com/progreizh/animaliste/controllers/AnimalControllerTest.kt
+++ src/test/kotlin/com/progreizh/animaliste/controllers/AnimalControllerTest.kt
@@ -3,8 +3,8 @@ package com.progreizh.animaliste.controllers
 import com.progreizh.animaliste.entities.Animal
 import com.progreizh.animaliste.repositories.AnimalRepository
 import org.bson.types.ObjectId
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -12,7 +12,9 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.postForEntity
 import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.util.*
 
@@ -29,35 +31,35 @@ class AnimalControllerTest @Autowired constructor(
 
     private val defaultAnimalId: String = ObjectId.get().toHexString()
 
+    private val defaultAnimal: Animal = Animal(
+        defaultAnimalId,
+        "123456789012345",
+        "123ABC",
+        "0000001",
+        Date(),
+        "Description physique",
+        true,
+        "Chat",
+        "Siamoix",
+        "Description attitude",
+        "Chiens",
+        "Enfants",
+        "Non",
+        "Oeufs",
+        "Mimic",
+        "Mimic est un chat",
+        "Imaginaire"
+    )
+
     @BeforeEach
     fun setUp() {
         animalRepository.deleteAll()
     }
 
-    private fun getRootUrl(): String? = "http://localhost:$port/animals"
+    private fun getRootUrl(): String = "http://localhost:$port/animals"
 
 
-    fun saveOneAnimal() = animalRepository.save(
-            Animal(
-                    defaultAnimalId,
-                    "123456789012345",
-                    "123ABC",
-                    "0000001",
-                    Date(),
-                    "Description physique",
-                    true,
-                    "Chat",
-                    "Siamoix",
-                    "Description attitude",
-                    "Chiens",
-                    "Enfants",
-                    "Non",
-                    "Oeufs",
-                    "Mimic",
-                    "Mimic est un chat",
-                    "Imaginaire"
-            )
-    )
+    fun saveOneAnimal() = animalRepository.save(defaultAnimal)
 
 
     @Test
@@ -87,4 +89,25 @@ class AnimalControllerTest @Autowired constructor(
         assertEquals(defaultAnimalId, response.body?.id)
     }
 
+    @Test
+    fun `should return single animal with new id`() {
+        val response = restTemplate.postForEntity<Animal>(getRootUrl(), defaultAnimal)
+
+        assertEquals(HttpStatus.CREATED, response.statusCode)
+        assertNotNull(response.body)
+        assertEquals(defaultAnimalId, response.body?.id)
+    }
+
+    @Test
+    fun `should return animal list without animal` (){
+        saveOneAnimal()
+        restTemplate.delete(getRootUrl()+"", defaultAnimalId)
+
+        val response = restTemplate.getForEntity(
+            getRootUrl() + "/$defaultAnimalId",
+            Animal::class.java
+        )
+
+        assertEquals(200, response.statusCode.value())
+    }
 }
